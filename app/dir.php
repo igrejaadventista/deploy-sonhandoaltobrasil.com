87695a9a675b20117c39e85fb458b8619a187fae
Simei Smoler <38670971+simeismoler@users.noreply.github.com>
2022-07-18 12:05:01 -0300

    Update dir.php

--- app/dir.php
+++ app/dir.php
@@ -8,6 +8,29 @@ echo  __DIR__ ;
 $dir    = '../../';
 $files1 = scandir($dir);
 
-print_r($files1);
 
+function dirToArray($dir) {
+  
+    $result = array();
+ 
+    $cdir = scandir($dir);
+    foreach ($cdir as $key => $value)
+    {
+       if (!in_array($value,array(".","..")))
+       {
+          if (is_dir($dir . DIRECTORY_SEPARATOR . $value))
+          {
+             $result[$value] = dirToArray($dir . DIRECTORY_SEPARATOR . $value);
+          }
+          else
+          {
+             $result[] = $value;
+          }
+       }
+    }
+   
+    return $result;
+ }
+
+dirToArray($dir);
 ?>
