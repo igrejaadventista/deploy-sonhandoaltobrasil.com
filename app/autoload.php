1f025c781ae2e7bfed1bbce39645a6436e4a97cc
Simei Smoler <38670971+simeismoler@users.noreply.github.com>
2022-07-18 14:00:39 -0300

    .

--- app/autoload.php
+++ app/autoload.php
@@ -6,3 +6,5 @@ require_once  __DIR__ . '../composer/autoload_real.php';
 //require_once implode([__DIR__, 'composer', 'autoload_real.php'], DIRECTORY_SEPARATOR);
 
 return ComposerAutoloaderInit887fe7c3e68b8fe16f44cca87b7bba27::getLoader();
+
+?>
