c84ad19536ce5ed46fcc726341c0d189f20ab6a7
Simei Smoler <38670971+simeismoler@users.noreply.github.com>
2022-07-20 09:35:40 -0300

    .

--- app/inscrevase.php
+++ app/inscrevase.php
@@ -53,7 +53,7 @@
 
 					<div class="form-result"></div>
 
-					<form class="mb-0 row" id="template-contactform" name="template-contactform" action="envio.php" method="post">
+					<form class="mb-0 row" id="template-contactform" name="template-contactform" action="include/form.php" method="post">
 
 						<div class="form-process">
 							<div class="css3-spinner">
